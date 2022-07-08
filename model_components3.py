4a56b47cd7fa11aaec464be9e8410229e0ec263c
daibiaoxuwu <dujluo@gmail.com>
2022-07-08 18:36:35 +0800

    working

--- model_components3.py
+++ model_components3.py
@@ -1,220 +1,145 @@
-# models.py
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import time
-import cv2
-import numpy as np
-import os
-from utils import *
-import math
-import sys  
-
-def norm(y):
-    for i in range(y.shape[1]):
-        y_abs = torch.abs(y[:,i])
-        y_abs_max = torch.tensor(
-            list(map(lambda x: torch.max(x), y_abs)))
-        y_abs_max = to_var(torch.unsqueeze(torch.unsqueeze(y_abs_max, 1), 2))
-        y[:,i] = torch.div(y[:,i], y_abs_max)
-    return y
-class classificationHybridModel3(nn.Module):
-    """Defines the architecture of the discriminator network.
-       Note: Both discriminators D_X and D_Y have the same architecture in this assignment.
-    """
-
+# models.py 
+ 
+import torch 
+import torch.nn as nn 
+import torch.nn.functional as F 
+import time 
+import cv2 
+import numpy as np 
+import os 
+from utils import * 
+import math 
+import sys   
+ 
+from complexPyTorch.complexLayers import ComplexBatchNorm2d, ComplexConv2d, ComplexLinear 
+from complexPyTorch.complexFunctions import complex_relu, complex_max_pool2d 
+ 
+from torch.nn.functional import relu, max_pool2d, tanh 
+def complex_tanh(input): 
+    return tanh(input.real).type(torch.complex64)+1j*tanh(input.imag).type(torch.complex64) 
+ 
+class classificationHybridModel3(nn.Module): 
+    """Defines the architecture of the discriminator network. 
+       Note: Both discriminators D_X and D_Y have the same architecture in this assignment. 
+    """ 
+ 
+    def __init__(self, conv_dim_in=2, conv_dim_out=128, conv_dim_lstm=1024):  
+        super(classificationHybridModel3, self).__init__()  
+  
+        self.out_size = 1  
+        self.conv1 = nn.Conv2d(2, 16, (3, 3), stride=(2, 2), padding=(1, 1))  
+        self.pool1 = nn.MaxPool2d((2, 2), stride=(2, 2))  
+        self.dense = nn.Linear(conv_dim_lstm * 4, conv_dim_out * 2)  
+        self.fcn1 = nn.Linear(conv_dim_out * 2, conv_dim_out)  
+        self.fcn2 = nn.Linear(conv_dim_out, conv_dim_out)  
+        self.softmax = nn.Softmax(dim=1)  
+  
+        self.drop1 = nn.Dropout(0.1)  
+        self.drop2 = nn.Dropout(0.1)  
+        self.act = nn.LeakyReLU()  
+  
+    def forward(self, x):  
+        out = self.act(self.conv1(x))  
+        out = self.pool1(out)  
+        out = out.view(out.size(0), -1)  
+  
+        out = self.act(self.dense(out))  
+        out = self.drop2(out)  
+  
+        out = self.act(self.fcn1(out))  
+        out = self.drop1(out)  
+        out = self.fcn2(out)  
+        return out   
+ 
+class maskCNNModel3(nn.Module): 
+    def __init__(self, opts): 
+        super(maskCNNModel3, self).__init__() 
+        self.opts = opts 
+ 
+        self.conv1 = ComplexConv2d(1, 64, 7, padding=3) 
+        self.bn2d1 = ComplexBatchNorm2d(64) 
+ 
+        self.conv21a = ComplexConv2d(64, 64, 5, padding=2) 
+        self.bn2d21a = ComplexBatchNorm2d(64) 
+        self.conv21b = ComplexConv2d(64, 64, 3, padding=1) 
+        self.bn2d21b = ComplexBatchNorm2d(64) 
+        self.conv21c = ComplexConv2d(64, 64, 3, padding=1) 
+        self.bn2d21c = ComplexBatchNorm2d(64) 
+        self.conv22b = ComplexConv2d(64, 64, 3, padding=1) 
+        self.bn2d22b = ComplexBatchNorm2d(64) 
+        self.conv22c = ComplexConv2d(64, 64, 3, padding=1) 
+        self.bn2d22c = ComplexBatchNorm2d(64) 
+ 
+        self.conv3a = ComplexConv2d(64, 64, 5, padding=2) 
+        self.bn2d3a = ComplexBatchNorm2d(64) 
+        self.conv3b = ComplexConv2d(64, 64, 7, padding=3) 
+        self.bn2d3b = ComplexBatchNorm2d(64) 
+        self.conv3c = ComplexConv2d(64, 1, 5, padding=2) 
+        self.bn2d3c = ComplexBatchNorm2d(1) 
+ 
+        #self.fc1 = ComplexLinear(opts.conv_dim_lstm , opts.fc1_dim) 
+        #self.fc2 = ComplexLinear(opts.fc1_dim, opts.freq_size) 
+ 
+    def forward(self, xslist): 
+        outlist = []
+        for xs in xslist:
+            outs = (xs[:,0]+1j*xs[:,1]).unsqueeze(1) 
+                     
+            # CNN_1 
+            outs = self.conv1(outs) 
+            #outs = self.bn2d1(outs) 
+            outs = complex_relu(outs) 
      
-    def __init__(self, conv_dim_in=2, conv_dim_out=128, conv_dim_lstm=1024): 
-        super(classificationHybridModel3, self).__init__() 
- 
-        self.out_size = 1 
-        self.conv1 = nn.Conv2d(2, 16, (3, 3), stride=(2, 2), padding=(1, 1)) 
-        self.pool1 = nn.MaxPool2d((2, 2), stride=(2, 2)) 
-        self.dense = nn.Linear(conv_dim_lstm * 4, conv_dim_out * 2) 
-        self.fcn1 = nn.Linear(conv_dim_out * 2, conv_dim_out) 
-        self.fcn2 = nn.Linear(conv_dim_out, conv_dim_out) 
-        self.softmax = nn.Softmax(dim=1) 
- 
-        self.drop1 = nn.Dropout(0.1) 
-        self.drop2 = nn.Dropout(0.1) 
-        self.act = nn.LeakyReLU() 
- 
-    def forward(self, x): 
-        out = self.act(self.conv1(x)) 
-        out = self.pool1(out) 
-        out = out.view(out.size(0), -1) 
- 
-        out = self.act(self.dense(out)) 
-        out = self.drop2(out) 
- 
-        out = self.act(self.fcn1(out)) 
-        out = self.drop1(out) 
-        out = self.fcn2(out) 
-        return out 
- 
+            # CNN_2 
+            ''' 
+            out_abs = torch.mean(torch.stack([torch.abs(out) for out in outs],0),0) 
+            outavg = [out * (out_abs / torch.abs(out)) for out in outs] 
+            out_max = torch.max(torch.stack([torch.abs(out) for out in outs],0),0)[0] 
+            outmax = [out * (out_max / torch.abs(out)) for out in outs] 
+            out_min = torch.min(torch.stack([torch.abs(out) for out in outs],0),0)[0] 
+            outmin = [out * (out_min / torch.abs(out)) for out in outs] 
+            ''' 
+            ''' 
+            out_max = torch.max(torch.stack([torch.abs(out) for out in outs],0),0)[0] 
+            outmax = [out * (out_max / (torch.abs(out)+1e-6)) for out in outs] 
+     
+            outs = torch.cat((outs[idx], outmax[idx], xs[idx]),1) for idx in range(self.opts.stack_imgs)] 
+            ''' 
+     
+            outs = self.conv21a(outs) 
+            #outs = self.bn2d21a(outs) 
+            outs = complex_relu(outs) 
+            outs = self.conv21b(outs) 
+            #outs = self.bn2d21b(outs) 
+            outs = complex_relu(outs) 
+            outs = self.conv21c(outs) 
+            #outs = self.bn2d21c(outs) 
+            outs = complex_relu(outs) 
+     
+            ''' 
+            out_max = torch.max(torch.stack([torch.abs(out) for out in outs],0),0)[0] 
+            outmax = [out * (out_max /  (torch.abs(out)+1e-6)) for out in outs] 
+     
+            outs = torch.cat((outs[idx], outmax[idx], xs[idx]),1) for idx in range(self.opts.stack_imgs)] 
+            ''' 
+     
+            outs = self.conv3c(outs) 
+            #outs = self.bn2d3c(outs) 
+            #outs = complex_tanh(outs) 
+     
+            ''' 
+            outs = x.transpose(1, 2) for x in outs] 
+            outs = x.reshape(x.size(0), x.size(1), -1) for x in outs] 
+            outs = self.fc1(outs) 
+            outs = complex_relu(outs) 
+            outs = self.fc2(outs) 
+            outs = complex_relu(outs) 
+     
+            #Final 
+            outs = x.view(x.size(0), x.size(1), 1, -1) for x in outs] 
+            outs = x.transpose(1, 2).transpose(2, 3).contiguous() for x in outs]''' 
+     
+            outs = torch.stack(( outs.real, outs.imag), 1).squeeze()
+            outlist.append(outs)
+        return outlist
 
-
-class maskCNNModel3(nn.Module):
-    def __init__(self, opts):
-        super(maskCNNModel3, self).__init__()
-        self.opts = opts
-        self.writeindex = 0
-
-        self.conv1 = nn.Sequential(
-            # cnn1
-            nn.ZeroPad2d(2),
-            nn.Conv2d(opts.x_image_channel, 256, kernel_size=(5, 5), dilation=(1, 1)),
-            nn.BatchNorm2d(256), nn.LeakyReLU(),
-
-            )
-
-        self.conv2 = []
-        for i in range(opts.layer_cnt):
-            self.conv2.append( nn.Sequential(
-
-                # cnn3
-                nn.Conv2d(256*4+2, 64, kernel_size=(1, 1), dilation=(1, 1)),
-                nn.BatchNorm2d(64), nn.LeakyReLU(),
-
-                nn.ZeroPad2d(1),
-                nn.Conv2d(64, 64, kernel_size=(3, 3), dilation=(1, 1)),
-                nn.BatchNorm2d(64), nn.LeakyReLU(),
-
-                nn.Conv2d(64, 256, kernel_size=(1, 1), dilation=(1, 1)),
-                nn.BatchNorm2d(256)))
-        self.conv2 = nn.ModuleList(self.conv2)
-
-
-        self.conv3 = nn.Sequential(
-
-            # cnn3
-            nn.ZeroPad2d(2),
-            nn.Conv2d(256, 64, kernel_size=(5, 5), dilation=(1, 1)),
-            nn.BatchNorm2d(64), nn.LeakyReLU(),
-
-            # cnn4
-            nn.ZeroPad2d((2, 2, 4, 4)),
-            nn.Conv2d(64, 64, kernel_size=(5, 5), dilation=(2, 1)),
-            nn.BatchNorm2d(64), nn.LeakyReLU(),
-
-            # cnn8
-            nn.Conv2d(64, 8, kernel_size=(1, 1), dilation=(1, 1)),
-            nn.BatchNorm2d(8))
-
-        #self.lstm = nn.LSTM( opts.conv_dim_lstm, opts.lstm_dim, batch_first=True, bidirectional=True)
-        self.fc1 = nn.Linear(opts.conv_dim_lstm , opts.fc1_dim)
-        self.fc2 = nn.Linear(opts.fc1_dim, opts.freq_size * opts.out_channel)
-        self.final = nn.Sigmoid()
-
-    def merge_images(self, sources, opts):
-        b, c, h, w = sources[0].shape
-        row = int(np.sqrt(c))
-        column = math.ceil(c / row)
-        merged = np.zeros([row * h * opts.stack_imgs, column * w])
-        for stack_idx in range(len(sources)):
-            for idx, s in enumerate(sources[stack_idx][0]):
-                i = idx // column
-                j = idx % column
-                merged[(i * opts.stack_imgs + stack_idx) * h:(i * opts.stack_imgs + 1 + stack_idx) * h, j * w: (j + 1) * w] = s
-        return merged.transpose(0,1)
-
-
-    def save_samples(self, fixed_Y, opts, lvidx):
-        """Saves samples from both generators X->Y and Y->X.
-        """
-        Y = [to_data(i) for i in fixed_Y]
-
-        merged = self.merge_images(Y, opts)
-
-        path = os.path.join(opts.checkpoint_dir, 'sample-Y'+str(lvidx)+'.png')
-        merged = (merged - np.amin(merged)) / (np.amax(merged) - np.amin(merged)) * 255
-        merged = cv2.flip(merged, 0)
-        cv2.imwrite(path, merged)
-        print('SAMPLE: {}'.format(path))
-
-    def forward(self, xs):
-        self.writeindex+=1
-        xsnew = [x.transpose(2, 3) for x in xs]
-        
-        # CNN_1
-        outs = [self.conv1(x) for x in xsnew]
-
-
-        for i in range(self.opts.layer_cnt):
-
-            # Merge
-            outss = torch.stack(outs,1)
-            outavg = torch.mean(outss,1)
-            outmax = torch.max(outss,1)[0]-0.01*outavg
-            outmin = torch.min(outss,1)[0]+0.01*outavg
-            #outmax *= 0
-            #outmin *= 0
-            #self.save_samples(outs, self.opts, str(i)+'outs')
-            #self.save_samples([outavg,outmax,outmin], self.opts, str(i)+'avgs')
-            '''
-            out_abs = torch.mean(torch.stack([torch.abs(out[:,0]+1j*out[:,1]) for out in outs],0),0)
-            outavg = [out * (out_abs / torch.abs(out[:,0]+1j*out[:,1])).unsqueeze(1).repeat(1, 2, 1, 1) for out in outs]
-            out_max = torch.max(torch.stack([torch.abs(out[:,0]+1j*out[:,1]) for out in outs],0),0)[0]
-            outmax = [out * (out_abs / torch.abs(out[:,0]+1j*out[:,1])).unsqueeze(1).repeat(1, 2, 1, 1) for out in outs]-0.01*outavg
-            out_min = torch.min(torch.stack([torch.abs(out[:,0]+1j*out[:,1]) for out in outs],0),0)[0]
-            outmin = [out * (out_abs / torch.abs(out[:,0]+1j*out[:,1])).unsqueeze(1).repeat(1, 2, 1, 1) for out in outs]+0.01*outavg'''
-
-            outs2 = []
-            for idx in range(len(xs)):
-                # Distribute
-                x = torch.cat((outs[idx],outs[(idx+1)%len(outs)],outs[(idx+2)%len(outs)],outs[(idx+3)%len(outs)],xsnew[idx]),1)
-
-                # CNN_2
-                out1 = self.conv2[i](x)
-                outs2.append( out1 + outs[idx] )
-                if idx>-1:continue
-                '''
-                for k in range(len(self.conv2[i])):
-                    print(type(self.conv2[i][k]))
-                    if not isinstance(self.conv2[i][k], nn.Conv2d): continue
-                    sources = self.conv2[i][k].weight.data
-                    print(k,sources.shape)
-                    continue'''
-                sources = to_data(self.conv2[i][0].weight.data)
-                print(sources.shape)
-                print('orig ',np.mean(sources[:,:256]))
-                print('max  ',np.mean(sources[:,256:512]))
-                print('min  ' ,np.mean(sources[:,512:768]))
-                print('input',np.mean(sources[:,768:]))
-                for k in range(4,5):
-
-                    sources = self.conv2[i][k].weight.data.cpu()
-                    b, c, h, w = sources.shape
-                    print(sources.shape)
-                    merged = np.zeros([b * (h+1), c * (w+1)])
-                    for ii in range(b):
-                        for j in range(c):
-                            merged[ii * (h+1):(ii + 1) * (h+1)-1, j * (w+1): (j + 1) * (w+1)-1] = sources[ii, j]
-                    #merged = to_data(sources.squeeze())
-                    path = os.path.join(self.opts.checkpoint_dir, 'weights-'+str(i)+'-'+str(k)+'.png')
-                    merged = (merged - np.amin(merged)) / (np.amax(merged) - np.amin(merged)) * 255
-                    #merged = cv2.flip(merged, 0)
-                    cv2.imwrite(path, merged)
-                    print('SAMPLE: {}'.format(path))
-
-
-            outs = outs2
-        #sys.exit(1)
-
-        for idx in range(len(xs)):
-            out = self.conv3(outs[idx]).transpose(1, 2)
-            out = out.reshape(out.size(0), out.size(1), -1)
-            #out, _ = self.lstm(out)
-            out = nn.LeakyReLU()(out)
-            out = self.fc1(out)
-            out = nn.LeakyReLU()(out)
-            out = self.fc2(out)
-            out = self.final(out)
-
-            out = out.view(out.size(0), out.size(1), self.opts.out_channel, -1)
-            out = out.transpose(1, 2)
-            out = out.transpose(2, 3)
-            #outs[idx] = (out + xs[idx]).contiguous() ##change
-            outs[idx] = (out).contiguous()
-        return outs
